6b4fe82c73fe754fe9ff6f5fe60cd0d2f74e1544
ReACTor72 <134897554+ReACTor72@users.noreply.github.com>
2025-09-21 01:44:54 -0400

    Menu: Se crea el menu principal del programa

---
+++ DemoVector.py
@@ -0,0 +1,48 @@
+import random
+import os
+
+def main():
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    Limite_n_valores_vector = 20
+    min_val_aleatorio = 1
+    max_val_aleatorio = 500
+    
+    resultados = {}
+    opcion_de_menu = '9'
+
+    vector_num_aleatorios = []
+
+    while opcion_de_menu != '4':
+        contador_temporal_resultados = 0
+
+        print("\n\t" + "~"*60, f"N={Limite_n_valores_vector}, Valores (Min={min_val_aleatorio}, Max={max_val_aleatorio})")
+        print("\n\tVector generado:", vector_num_aleatorios)
+
+        print("\n\t\tMENÚ DE OPCIONES")
+        print("\t" + "~"*60)
+        print(f"\n\t1. Suma de números del vector: \t\t\t{resultados.get('suma', 'No calculado')}")
+        print(f"\t2. Contar los números que no son primos: \t{resultados.get('no_primos', 'No calculado')}")
+        print(f"\t3. Contar los números capicúas: \t\t{resultados.get('capicuas', 'No calculado')}")
+        print("\t4. Salir.")
+        print("\t" + "~"*60)
+        opcion_de_menu = input("\n\tSeleccione una opción: ")
+
+        if opcion_de_menu == '1':
+            print(f"\n-> Suma calculada: {resultados['suma']}")
+
+        elif opcion_de_menu == '2':
+            print(f"\n-> Cantidad de no primos: {resultados['no_primos']}")
+            
+        elif opcion_de_menu == '3':
+            print(f"\n-> Cantidad de capicúas: {resultados['capicuas']}")
+
+        elif opcion_de_menu == '4':
+            print("\n\t-- Fin del ejercicio --")
+            
+        else:
+            print("* "*15, "Opción no válida. Intente de nuevo.")
+
+if __name__ == "__main__":
+    main()
